69c8f81c6e93bee494b7d289673cc55b5352941e
mabarcaj <mabarcaj1@gmail.com>
2023-06-20 12:58:43 -0400

    route tastely_test

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export function Navigation() {
     return (
         <div className="flex justify-between py-3 font-delius-swash-caps">
-            <Link to="/tastely">
+            <Link to="/tastely_test/">
                 <h1 className="font-bold text-3xl mb-4">Tastely</h1>
             </Link>
             <button className="bg-indigo-500 px-3 py-2 rounded-lg text-white">
